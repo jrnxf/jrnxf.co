ceed51af4fcc99137cb935c4b9c13e686041c31d
Colby Thomas <colby@jrnxf.co>
2023-04-29 21:55:23 -0600

    pwa compat

--- app/head.tsx
+++ app/head.tsx
@@ -34,6 +34,8 @@ export default function Head() {
         rel="icon"
         href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🌱</text></svg>"
       />
+      {/* PWACompat is a library that brings the Web App Manifest to non-compliant browsers for better Progressive Web Apps. This mostly means creating splash screens and icons for Mobile Safari, as well as supporting IE/Edge's Pinned Sites feature. */}
+      <script async src="https://unpkg.com/pwacompat" crossOrigin="anonymous" />
     </>
   )
 }
